7c1c582a3498b4705d16515a8e46579aee6a1276
burnett2 <bernettkayembe8@gmail.com>
2024-05-27 15:36:43 +0200

    Add files via upload

---
+++ JavaApplication15/src/javaapplication15/JavaApplication15.java
@@ -0,0 +1,18 @@
+package javaapplication15;
+
+public class JavaApplication15 {
+    
+    static void displayHelloWorld(){
+      System.out.println("helloworld");
+    }
+    static void displayKayembeBurnett(){
+        System.out.println("kayembeburnett");
+    
+    }
+    
+    public static void main(String[] args) {
+       displayHelloWorld();
+       displayKayembeBurnett();
+    }
+    
+}
